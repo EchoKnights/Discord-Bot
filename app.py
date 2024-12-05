a623c4bac58913b843dc3f20d3839d104108d7d3
EchoKnights <alytamerg@gmail.com>
2024-12-05 12:53:53 +0200

    Second Init Commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 import discord as dc
-import Authcode
+import GitIgnorables.Authcode as Authcode
 from discord import app_commands
 from discord.ext import commands
 import log, DACommands
